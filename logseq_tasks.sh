f1bb37822e81b0f9809f22e6fc3d1e61139574a2
Dennis Faucher <dennis@faucher.net>
2026-02-22 17:59:28 -0500

    Add files via upload

--- logseq_tasks.sh
+++ logseq_tasks.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
-#echo "Have you reindexed logseq first?"
-#read ans
-#echo "Getting Logseq Tasks and Sorting..."
-#lq q task -g Logseq -C|grep -e "InsideToDo" -e "OutsideTodo" -e "ResearchToDo"|sort > ~/Downloads/logseq_tasks.txt
-#echo "Opening file in TextEdit..."
-#open -a TextEdit ~/Downloads/logseq_tasks.txt
-
 echo "Have you reindexed logseq first?"
 read ans
 echo "Getting Logseq Tasks and Sorting..."
+
 echo "=============" > ~/Downloads/logseq_tasks.txt
 echo "Inside To DO" >> ~/Downloads/logseq_tasks.txt
 echo "=============" >> ~/Downloads/logseq_tasks.txt
-lq q task -g ResilioLogseq -C|grep -e "InsideToDo"  | sort >> ~/Downloads/logseq_tasks.txt
+#lq q task -g ResilioLogseq -C|grep -e "InsideToDo"  | sort >> ~/Downloads/logseq_tasks.txt
+lq q task -g ResilioLogseq -C|grep -e "InsideToDo"  | sort | sed 's/- TODO #InsideToDo //g' >> ~/Downloads/logseq_tasks.txt
 echo " " >> ~/Downloads/logseq_tasks.txt
+
 echo "=============" >> ~/Downloads/logseq_tasks.txt
 echo "Outside To DO" >> ~/Downloads/logseq_tasks.txt
 echo "=============" >> ~/Downloads/logseq_tasks.txt
-lq q task -g ResilioLogseq -C|grep  -e "OutsideTodo" | sort >> ~/Downloads/logseq_tasks.txt
+#lq q task -g ResilioLogseq -C|grep  -e "OutsideTodo" | sort >> ~/Downloads/logseq_tasks.txt
+lq q task -g ResilioLogseq -C|grep  -e "OutsideTodo" | sort | sed 's/- TODO #OutsideTodo //g' >> ~/Downloads/logseq_tasks.txt
 echo " " >> ~/Downloads/logseq_tasks.txt
+
 echo "=============" >> ~/Downloads/logseq_tasks.txt
 echo "Research To DO" >> ~/Downloads/logseq_tasks.txt
 echo "=============" >> ~/Downloads/logseq_tasks.txt
-lq q task -g ResilioLogseq -C|grep -e  "ResearchToDo" | sort >> ~/Downloads/logseq_tasks.txt
-#lq q task -g Logseq -C|grep -e "ADPToDo" -e "CencoraToDo" -e "RTXToDo" -e "WWTToDo" | sort > ~/Downloads/logseq_tasks.txt
+#lq q task -g ResilioLogseq -C|grep -e  "ResearchToDo" | sort >> ~/Downloads/logseq_tasks.txt
+lq q task -g ResilioLogseq -C|grep -e  "ResearchToDo" | sort | sed 's/- TODO #ResearchToDo //g' >> ~/Downloads/logseq_tasks.txt
+
 echo "Opening file in TextEdit..."
 open -a TextEdit ~/Downloads/logseq_tasks.txt
