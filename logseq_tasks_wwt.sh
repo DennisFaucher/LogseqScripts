08f8d9ce470cbdd7a019e6678b53de7e18a3c1b5
Dennis Faucher <dennis@faucher.net>
2026-02-22 18:05:56 -0500

    Add files via upload

--- logseq_tasks_wwt.sh
+++ logseq_tasks_wwt.sh
@@ -6,26 +6,21 @@ echo "Getting Logseq Tasks and Sorting..."
 echo "=============" > ~/Downloads/logseq_tasks_wwt.txt
 echo "ADP To DO" >> ~/Downloads/logseq_tasks_wwt.txt
 echo "=============" >> ~/Downloads/logseq_tasks_wwt.txt
-#lq q task -g Logseq -C|grep -e "ADPToDo"  | sort >> ~/Downloads/logseq_tasks_wwt.txt
-lq q task -g ResilioLogseq -C|grep -e "ADPToDo"  | sort >> ~/Downloads/logseq_tasks_wwt.txt
+lq q task -g ResilioLogseq -C|grep -e "ADPToDo"  | sort | sed 's/- TODO #ADPToDo //g' >> ~/Downloads/logseq_tasks_wwt.txt
 echo " " >> ~/Downloads/logseq_tasks_wwt.txt
 echo "=============" >> ~/Downloads/logseq_tasks_wwt.txt
 echo "Cencora To DO" >> ~/Downloads/logseq_tasks_wwt.txt
 echo "=============" >> ~/Downloads/logseq_tasks_wwt.txt
-#lq q task -g Logseq -C|grep  -e "CencoraToDo" | sort >> ~/Downloads/logseq_tasks_wwt.txt
-lq q task -g ResilioLogseq -C|grep  -e "CencoraToDo" | sort >> ~/Downloads/logseq_tasks_wwt.txt
+lq q task -g ResilioLogseq -C|grep  -e "CencoraToDo" | sort | sed 's/- TODO #CencoraToDo //g' >> ~/Downloads/logseq_tasks_wwt.txt
 echo " " >> ~/Downloads/logseq_tasks_wwt.txt
 echo "=============" >> ~/Downloads/logseq_tasks_wwt.txt
 echo "RTX To DO" >> ~/Downloads/logseq_tasks_wwt.txt
 echo "=============" >> ~/Downloads/logseq_tasks_wwt.txt
-#lq q task -g Logseq -C|grep -e  "RTXToDo" | sort >> ~/Downloads/logseq_tasks_wwt.txt
-lq q task -g ResilioLogseq -C|grep -e  "RTXToDo" | sort >> ~/Downloads/logseq_tasks_wwt.txt
+lq q task -g ResilioLogseq -C|grep -e  "RTXToDo" | sort | sed 's/- TODO #RTXToDo //g' >> ~/Downloads/logseq_tasks_wwt.txt
+echo " " >> ~/Downloads/logseq_tasks_wwt.txt
 echo "=============" >> ~/Downloads/logseq_tasks_wwt.txt
 echo "WWT To DO" >> ~/Downloads/logseq_tasks_wwt.txt
 echo "=============" >> ~/Downloads/logseq_tasks_wwt.txt
-#lq q task -g Logseq -C|grep  "WWTToDo" | sort >> ~/Downloads/logseq_tasks_wwt.txt
-lq q task -g ResilioLogseq -C|grep  "WWTToDo" | sort >> ~/Downloads/logseq_tasks_wwt.txt
-echo "=============" >> ~/Downloads/logseq_tasks_wwt.txt
-#lq q task -g Logseq -C|grep -e "ADPToDo" -e "CencoraToDo" -e "RTXToDo" -e "WWTToDo" | sort > ~/Downloads/logseq_tasks_wwt.txt
+lq q task -g ResilioLogseq -C|grep  "WWTToDo" | sort | sed 's/- TODO #WWTToDo //g' >> ~/Downloads/logseq_tasks_wwt.txt
 echo "Opening file in TextEdit..."
 open -a TextEdit ~/Downloads/logseq_tasks_wwt.txt
